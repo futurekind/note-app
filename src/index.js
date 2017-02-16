4bd07aae1b30a8b83c965873c7e2e654df63001b
Fabian Trinler <fabian.trinler@blateral.com>
2017-02-16 09:55:11 +0100

    Fixing tests

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
-import Checkbox from './components/Checkbox'
-
 render(
     <h1>Hallo</h1>,
     document.getElementById('root')
