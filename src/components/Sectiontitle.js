5cfaccf436d035b763861be4dcb099a411692a5b
Fabian Trinler <fabian.trinler@blateral.com>
2017-02-20 11:54:04 +0100

    Adding Sectiontitle

---
+++ src/components/Sectiontitle.js
@@ -0,0 +1,46 @@
+import React, {PropTypes} from 'react'
+import styled from 'styled-components';
+
+import Icon from './Icon';
+import Divider from './Divider';
+
+const View =  styled.h2`
+    margin: 0;
+`
+
+const Body = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const Label = styled.div`
+    padding: 20px 0;
+    flex: 1;
+`
+
+const ViewIcon = styled.div`
+    padding: 20px;
+`
+
+const Sectiontitle = ({
+    iconId,
+    children
+}) => {
+    return (
+        <View>
+            <Body>
+                <Label>{ children }</Label>
+                { iconId && <ViewIcon>
+                    <Icon id={ iconId } />   
+                </ViewIcon> }
+            </Body>
+            <Divider />
+        </View>
+    )
+}
+
+Sectiontitle.propTypes = {
+    iconId: PropTypes.string,
+}
+
+export default Sectiontitle
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
-import Test from './components/Pagetitle';
+import Test from './components/Sectiontitle';
 
 render(
-    <Test>Hallo</Test>,
+    <Test iconId="edit">Hallo</Test>,
     document.getElementById('root')
 );
