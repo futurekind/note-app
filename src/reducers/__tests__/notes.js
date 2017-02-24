fcbeb3c13b26142485589af1b69c0b0261bf80ee
Fabian Trinler <fabian.trinler@blateral.com>
2017-02-24 10:09:14 +0100

    Adding reducer for notes

--- src/reducers/__tests__/notes.js
+++ src/reducers/__tests__/notes.js
@@ -30,4 +30,52 @@ describe('Notes reducer', () => {
         })
     })
 
+    it(`handles ${actions.types.NOTES__EDIT}`, () => {
+        const nextState = reducer(state, actions.create({
+            id: 'some-id',
+            updatedAt: '2017-02-23T16:19:09.984Z',
+            title: 'some title'
+        }))
+
+        const nextState2 = reducer(nextState, actions.edit('some-id', {
+            title: 'new title',
+            foo: 'bar'
+        }))
+        
+        expect(nextState2.entities['some-id'].title).toBe('new title')
+    })
+
+    it(`handles ${actions.types.NOTES__DELETE}`, () => {
+        const nextState = reducer(state, actions.create({
+            id: 'some-id',
+            updatedAt: '2017-02-23T16:19:09.984Z',
+            title: 'some title'
+        }))
+
+        const nextState2 = reducer(nextState, actions.create({
+            id: 'some-id2',
+            updatedAt: '2017-02-23T16:19:09.984Z',
+            title: 'some title2'
+        }))
+
+        const nextState3 = reducer(nextState2, actions.remove('some-id'))
+        
+        expect(nextState3.results).toEqual(['some-id2'])
+        expect(nextState3.entities['some-id']).toBeUndefined()
+    })
+
+    it(`handles ${actions.types.NOTES__ACTIVE}`, () => {
+        const nextState = reducer(state, actions.setActive('some-id'))
+        expect(nextState.active).toBe('some-id')
+    })
+
+    it(`handles ${actions.types.NOTES__OPEN}`, () => {
+        let nextState = reducer(state, actions.toggleOpen())
+        expect(nextState.open).toBe(true)
+
+        nextState = reducer(nextState, actions.toggleOpen())
+        expect(nextState.open).toBe(false)
+        
+    })
+
 })
--- src/reducers/notes.js
+++ src/reducers/notes.js
@@ -23,6 +23,47 @@ export default (state = initialState, action) => {
                 }
             }
 
+        case types.NOTES__EDIT:
+            return {
+                ...state,
+                entities: {
+                    [action.id]: {
+                        ...state.entities[action.id],
+                        ...action.payload
+                    }
+                }
+            }
+
+        case types.NOTES__DELETE:
+            const indexToDelete = state.results.indexOf(action.id)
+            return {
+                ...state,
+                results: [
+                    ...state.results.slice(0, indexToDelete),
+                    ...state.results.slice(indexToDelete + 1)
+                ],
+                entities: Object.keys(state.entities).reduce((nextEntities, key) => {
+                    if(key === action.id) return nextEntities
+
+                    return {
+                        ...nextEntities,
+                        ...state.entities[key]
+                    }
+                }, {})
+            }
+
+        case types.NOTES__ACTIVE:
+            return {
+                ...state,
+                active: action.id
+            }
+
+        case types.NOTES__OPEN:
+            return {
+                ...state,
+                open: !state.open
+            }
+
         default:
             return state
 
